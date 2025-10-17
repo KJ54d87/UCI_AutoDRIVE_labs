a035f9ceb2f486a51badfd7522b7fdbd6f043a47
KJ54d87 <75508753+KJ54d87@users.noreply.github.com>
2025-10-17 12:32:38 -0700

    Update lab1.py

--- src/lab1/lab1.py
+++ src/lab1/lab1.py
@@ -6,6 +6,7 @@ import eventlet
 from flask import Flask
 import autodrive
 import math
+import warnings
 
 ################################################################################
 
@@ -33,23 +34,45 @@ def dist(a, b, theta):
     return distance
 
 last_dif = 0
-p = .45
-d = .01
+p = .5
+d = .17
 
 def determineTurn(lidar_data):
     global last_dif
     
     try :
-        right_dist = dist(lidar_data[280], lidar_data[200], 20 * RADIANS_PER_ENTRY)
-        left_dist = dist(lidar_data[800], lidar_data[880], 20 * RADIANS_PER_ENTRY)
+        right_dist = dist(lidar_data[260], lidar_data[180], 20 * RADIANS_PER_ENTRY)
+        left_dist = dist(lidar_data[820], lidar_data[900], 20 * RADIANS_PER_ENTRY)
         print(left_dist, right_dist, left_dist - right_dist)    
         k = left_dist - right_dist
         der = k - last_dif
         decision = p*k + d*der
         last_dif = k 
-    except:
-        decision = 0
+    except Exception:
+        decision = -.1
+        return decision
     return decision
+
+acceleration_cap = .5
+accel_floor = .25
+accel = acceleration_cap
+
+def determineThrottle(steering):
+    global accel
+    mag = abs(steering)
+    if (accel < acceleration_cap):
+        accel = accel + .05
+    if mag > .12:
+        accel = accel - .1
+    if (accel < accel_floor):
+        accel = accel_floor
+    return accel
+
+def crash(f1tenth):
+    if (f1tenth.lidar_range_array[540] < .5):
+        f1tenth.throttle_command = -.1
+        f1tenth.steering_command = 10
+
 
 # Registering "Bridge" event handler for the server
 @sio.on("Bridge")
@@ -59,10 +82,9 @@ def bridge(sid, data):
         f1tenth_1.parse_data(data, True)
 
         # Vehicle control
-        f1tenth_1.throttle_command = 0.23  # [-1, 1]
         f1tenth_1.steering_command = determineTurn(f1tenth_1.lidar_range_array)  # generate your steering command 
-
-
+        f1tenth_1.throttle_command = determineThrottle(f1tenth_1.steering_command)  # [-1, 1]
+        crash(f1tenth_1)
         ########################################################################
 
         json_msg = f1tenth_1.generate_commands()
@@ -76,6 +98,7 @@ def bridge(sid, data):
 ################################################################################
 
 if __name__ == "__main__":
+    warnings.filterwarnings("error")
     app = socketio.Middleware(
         sio, app
     )  # Wrap flask application with socketio's middleware
